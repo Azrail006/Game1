bd47c12b5cf149691975e359ab4a5213ca85619e
Azrail <azrailcecoev326@gmail.com>
2022-11-15 17:37:56 +0300

    swith

---
+++ Switch.js
@@ -0,0 +1,18 @@
+const getNumberExplanation = (number) => {
+  let result;
+  switch (number) {
+    case 666:
+      result = devil number;
+      break;
+    case 42:
+      result = answer for everything;
+      break;
+    case 7:
+      result = prime number;
+      break
+    default:
+      result = null;
+  }
+  return result;
+};
+export default getNumberExplanation;
