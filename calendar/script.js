005c1894932902186016a45eaf844d3ae54c8560
Azrail <azrailcecoev326@gmail.com>
2022-11-22 18:59:33 +0300

    Переделал

---
+++ calendar/script.js
@@ -0,0 +1,42 @@
+const input = document.querySelector('input');
+const button = document.querySelector('.btn')
+
+
+
+button.onclick = () => getWeekDayByNumber(parseInt(input.value));
+
+function getWeekDayByNumber(day) {
+    switch (day) {
+        case 1:
+            alert('Monday');
+            break;
+        case 2:
+            alert('Tuesday');
+            break;
+        case 3:
+            alert('Wednesday')
+            break;
+        case 4:
+            alert('Thursday')
+            break;
+        case 5:
+            alert('Friday')
+            break;
+        case 6:
+            alert('Aturday')
+            break;
+        case 7:
+            alert('Sunday')
+            break;
+        case 8:
+            alert('Перебор')
+            break;
+        default:
+            alert('Нет таких значений');
+
+    }
+}
+
+
+
+
--- project/name.js
+++ project/name.js
@@ -1,13 +1,23 @@
-const output = document.querySelector('#output');
-const promt = document.querySelector('#promt');
-const input = document.querySelector('#promt input');
 
+let playBox = document.querySelector("#playBox");
+let btnLess = document.querySelector("#btnLess");
+let btnGreater = document.querySelector("#btnGreater");
+const button =document.querySelector('.btn');
 
+button.click= () => getIsPensioner(parseInt(input.value));
 
 function getIsPensioner(age, gender) {
-    if (gender === 'male' && age > 60 || gender === 'female' && age > 55) {
+    if (gender === 'male' && age >= 60 || gender === 'female' && age >= 55) {
         return true;
-    }
+    } else {
     return false;
-}
+    }
+}; 
+
+btnStart.addEventListener("click", function () {
+    btnStart.style.display = "none";
+    playBox.style.display = "block";
+
+});
+
 
