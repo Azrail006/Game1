205043736722eb6750798456bf9bbc420edcd56a
Azrail <azrailcecoev326@gmail.com>
2022-11-28 20:35:56 +0300

    Add files via upload

---
+++ examples/sckript.js
@@ -0,0 +1,11 @@
+const number = document.querySelector('#number');
+const output = document.querySelector('#output');
+const button = document.querySelector('.btn');
+ 
+
+
+
+function determineNumberDigits (number) {
+  return String(number)
+
+}
