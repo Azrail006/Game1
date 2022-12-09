1c788a6cb76551f2b904ea284391d4e524bd8353
Azrail <azrailcecoev326@gmail.com>
2022-12-09 20:30:27 +0300

    добавил генерацию картинок

---
+++ NODE.js/node.js
@@ -0,0 +1,4 @@
+const sum = 1 + 2;
+console.log(sum);
+  console.log('Azrail')
+  console.log([4, 5, 6])
--- мой сайт/1 сайт/script.js
+++ мой сайт/1 сайт/script.js
@@ -1,12 +1,42 @@
-  async function meme() {
+//   async function meme() {
 
 
-let response = await fetch('https://api.imgflip.com/get_memes')
-let content = await response.json()
+// let response = await fetch('https://api.imgflip.com/get_memes')
+// let content = await response.json()
 
 
-// for (const sector in content.data.memes) {
-    console.log(content)
-//   }
-}
-meme()
+//  for (const sector in content.data.memes) {
+//     console.log(content)
+//    }
+// }
+// meme()
+
+
+const generatorMemeBtn = document.querySelector(".btn");
+const memeImage = document.querySelector(".img")
+const memeTitle = document.querySelector(".title");
+const memeAuthor = document.querySelector(".header__img");
+
+let memes = [];
+
+fetch("https://api.imgflip.com/get_memes")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      memes = data.data.memes;
+    });
+      
+
+generatorMemeBtn.addEventListener("click", () => generator(memes));
+
+
+const generator = (array) => {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  updateDetalis(array[randomIndex].url)
+}  
+
+const updateDetalis = (url, title, author) => {
+  memeImage.setAttribute("src", url);
+  memeTitle.innerHTML = title;
+  memeAuthor.innerHTML = author;
+};
